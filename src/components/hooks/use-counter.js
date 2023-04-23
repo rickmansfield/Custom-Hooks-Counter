4276a48594dd3f20c114e23e5b468f7584632c05
Rick <ricksmycodeguy@gmail.com>
2023-04-23 16:23:07 -0500

    return counter for use

--- src/components/hooks/use-counter.js
+++ src/components/hooks/use-counter.js
@@ -1,4 +1,5 @@
-import {useEffect, useState} from 'react'
+//hooks share logic not state. Each component gets it's own state. So if this counter is used in multiple components, each will have their own state. If you want to share state, you need to use a context. Thus you can have a conter that uses this component that counts UP and another that also uses this component that counts DOWN. This is a good example of how to share logic, but not state.
+import { useEffect, useState } from 'react'
 
 function useCounter() {
   const [counter, setCounter] = useState(0);
@@ -10,6 +11,8 @@ function useCounter() {
 
     return () => clearInterval(interval);
   }, []);
-}
+
+  return counter;
+};
 
 export default useCounter
