925e2207d9cd80c6e8c9508af2c323c77c19d376
Rick <ricksmycodeguy@gmail.com>
2023-04-23 16:23:23 -0500

    Refactor the ForwardCounter.js with the hook

--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -1,17 +1,20 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import useCounter from './hooks/use-counter';
 
 import Card from './Card';
 
 const ForwardCounter = () => {
-  const [counter, setCounter] = useState(0);
+  const counter = useCounter();
+  //without the hook I would need this code to get the same result:
+  // const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCounter((prevCounter) => prevCounter + 1);
+  //   }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return <Card>{counter}</Card>;
 };
